af441d009ae33ae5409346b59abcc02ee262816a
aldofusterturpin <aldoft@hotmail.com>
2020-05-04 03:02:31 +0200

    Add function to simulate the downloading of one book

--- download_Springer_books.py
+++ download_Springer_books.py
@@ -9,36 +9,88 @@
 import pathlib
 import os
 import requests
+
 import logging
+logging.basicConfig(level=logging.INFO)
+
 import selenium
 from selenium import webdriver
 
 
-def try_to_get_element_by_xpath(driver, xpath_to_find, url_book_details_page):
-    my_element = None
-    try :
-        my_element = driver.find_element_by_xpath(xpath_to_find)
-    except selenium.common.exceptions.NoSuchElementException as e:
-        logging.warning(f"{type(e)} raised when looking for this XPath: {xpath_to_find}, in this url: {url_book_details_page}")
-    finally:
-        return my_element
-
-
-def download_book_from_url(driver, url_book_details_page):
-    driver.get(url_book_details_page)
+def simulate_download_of_book(driver, url_of_book_details_page):
+    driver.get(url_of_book_details_page)
 
     title_xpath = "//*[@id='main-content']/article[1]/div/div/div[1]/div/div/div[1]/div[2]/h1"
-    book_title_element = driver.find_element_by_xpath(title_xpath)
+    book_title_element = try_to_get_element_by_xpath(driver, title_xpath, url_of_book_details_page)
     book_title = book_title_element.get_attribute('innerHTML').replace(" ", "_")
 
     download_button_element = None
     xpath_to_find = "//*[@id='main-content']/article[1]/div/div/div[2]/div[1]/a"
-    download_button_element = try_to_get_element_by_xpath(driver, xpath_to_find, url_book_details_page)
+    download_button_element = try_to_get_element_by_xpath(driver, xpath_to_find, url_of_book_details_page)
     if download_button_element is None:        
         xpath_to_find = "//*[@id='main-content']/article[1]/div/div/div[2]/div/div/a"
-        download_button_element = try_to_get_element_by_xpath(driver, xpath_to_find, url_book_details_page)
-    #     xpath_to_find = "//*[@id='main-content']/article[1]/div/div/div[2]/div/div/a/span[2]"
-    #     download_button_element = driver.try_to_get_element_by_xpath(driver, xpath_to_find, url_book_details_page)
+        download_button_element = try_to_get_element_by_xpath(driver, xpath_to_find, url_of_book_details_page)
+    
+    download_url = download_button_element.get_attribute('href')
+
+    response = requests.get(download_url)
+    if response.status_code == 200:
+        logging.info(f"In Url {url_of_book_details_page} everything is okay")
+    else:
+        logging.error(f"Can't get url {download_url} in the book detail page: {url_of_book_details_page}")
+
+
+def simulate_download_of_books(driver, url):
+    driver.get(url)
+    # for i in range(1, 11):
+    i = 1
+    xpath_to_find = f"//*[@id='results-list']/li[{i}]/div[2]/h2/a"
+    my_element = try_to_get_element_by_xpath(driver, xpath_to_find, url)
+    url_of_book_details_page = my_element.get_attribute('href')
+    simulate_download_of_book(driver, url_of_book_details_page)
+
+
+
+# if the element identified by xpath_to_find exists in the url: 
+#   returns True
+# else: 
+#   returns False
+def does_exist_element_identified_by_xpath(driver, xpath_to_find, url):
+    try :
+        my_element = driver.find_element_by_xpath(xpath_to_find)
+        return True
+    except selenium.common.exceptions.NoSuchElementException as e:
+        logging.warning(f"{type(e)} raised when looking for this XPath: {xpath_to_find}, in this url: {url}")
+        return False
+
+
+# if the element identified by xpath_to_find exists in the url: 
+#   the function will return an object pointing to the element
+# else: 
+#   it will return None indicating that the element doesn't exist
+def try_to_get_element_by_xpath(driver, xpath_to_find, url):
+    my_element = None
+    try :
+        my_element = driver.find_element_by_xpath(xpath_to_find)
+    except selenium.common.exceptions.NoSuchElementException as e:
+        logging.warning(f"{type(e)} raised when looking for this XPath: {xpath_to_find}, in this url: {url}")
+    finally:
+        return my_element
+
+
+def download_book_from_url(driver, url_of_book_details_page):
+    driver.get(url_of_book_details_page)
+
+    title_xpath = "//*[@id='main-content']/article[1]/div/div/div[1]/div/div/div[1]/div[2]/h1"
+    book_title_element = try_to_get_element_by_xpath(driver, title_xpath, url_of_book_details_page)
+    book_title = book_title_element.get_attribute('innerHTML').replace(" ", "_")
+
+    download_button_element = None
+    xpath_to_find = "//*[@id='main-content']/article[1]/div/div/div[2]/div[1]/a"
+    download_button_element = try_to_get_element_by_xpath(driver, xpath_to_find, url_of_book_details_page)
+    if download_button_element is None:        
+        xpath_to_find = "//*[@id='main-content']/article[1]/div/div/div[2]/div/div/a"
+        download_button_element = try_to_get_element_by_xpath(driver, xpath_to_find, url_of_book_details_page)
     
     download_url = download_button_element.get_attribute('href')
 
@@ -62,9 +114,9 @@ def create_destination_folder():
         os.mkdir(destination_path)
 
 
-def download_books_from_file_containing_urls(driver, file_name):
+def download_books_from_file_containing_urls(driver, file_name_that_contains_urls):
     unique_urls = set()
-    with open(file_name) as f:
+    with open(file_name_that_contains_urls) as f:
         for url in f:
             if url not in unique_urls:
                 unique_urls.add(url)
@@ -72,12 +124,14 @@ def download_books_from_file_containing_urls(driver, file_name):
 
 
 def main(): 
-    create_destination_folder()
+    # create_destination_folder()
 
     driver = webdriver.Chrome('./chromedriver')
 
-    file_name = "input.txt"
-    download_books_from_file_containing_urls(driver, file_name)
+    # file_name_that_contains_urls = "input.txt"
+    # download_books_from_file_containing_urls(driver, file_name_that_contains_urls)
+
+    simulate_download_of_books(driver, "https://link.springer.com/search?facet-content-type=\"Book\"&sortOrder=newestFirst&showAll=true&package=mat-covid19_textbooks")
     
     driver.close()
 
